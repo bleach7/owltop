9c4cbf8bfcd819de480fb4fccebf5734e50b67be
Daulet Tulepbergenov <daulet6368@gmail.com>
2023-07-06 18:05:38 +0600

    refactor: delete lorem

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,56 +9,6 @@ const Home = () => {
           Подборки лучших курсов и рейтинги, основанные на реальных отзывах.
         </Text>
       </header>
-      <div className="flex flex-col gap-y-20">
-        <p>
-          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Est velit
-          numquam incidunt nulla expedita quasi recusandae, quam dolores ullam
-          impedit, reprehenderit natus aperiam assumenda sapiente quod
-          cupiditate, cum labore eius.
-        </p>
-        <p>
-          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Est velit
-          numquam incidunt nulla expedita quasi recusandae, quam dolores ullam
-          impedit, reprehenderit natus aperiam assumenda sapiente quod
-          cupiditate, cum labore eius.
-        </p>
-        <p>
-          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Est velit
-          numquam incidunt nulla expedita quasi recusandae, quam dolores ullam
-          impedit, reprehenderit natus aperiam assumenda sapiente quod
-          cupiditate, cum labore eius.
-        </p>
-        <p>
-          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Est velit
-          numquam incidunt nulla expedita quasi recusandae, quam dolores ullam
-          impedit, reprehenderit natus aperiam assumenda sapiente quod
-          cupiditate, cum labore eius.
-        </p>
-        <p>
-          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Est velit
-          numquam incidunt nulla expedita quasi recusandae, quam dolores ullam
-          impedit, reprehenderit natus aperiam assumenda sapiente quod
-          cupiditate, cum labore eius.
-        </p>
-        <p>
-          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Est velit
-          numquam incidunt nulla expedita quasi recusandae, quam dolores ullam
-          impedit, reprehenderit natus aperiam assumenda sapiente quod
-          cupiditate, cum labore eius.
-        </p>
-        <p>
-          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Est velit
-          numquam incidunt nulla expedita quasi recusandae, quam dolores ullam
-          impedit, reprehenderit natus aperiam assumenda sapiente quod
-          cupiditate, cum labore eius.
-        </p>
-        <p>
-          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Est velit
-          numquam incidunt nulla expedita quasi recusandae, quam dolores ullam
-          impedit, reprehenderit natus aperiam assumenda sapiente quod
-          cupiditate, cum labore eius.
-        </p>
-      </div>
     </section>
   );
 };
