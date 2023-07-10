779916a9b79debd1be2508a68794c828d0136ce0
Daulet Tulepbergenov <daulet6368@gmail.com>
2023-07-11 01:23:14 +0600

    feat: update button-link

--- src/components/ui/ButtonLink/ButtonLink.tsx
+++ src/components/ui/ButtonLink/ButtonLink.tsx
@@ -3,7 +3,7 @@
 import { ArrowRightIcon } from "@/imgs/icons";
 import classNames from "classnames";
 import { motion } from "framer-motion";
-import Link from "next/link";
+import { useRouter } from "next/navigation";
 import { IButtonLink } from "./ButtonLink.interface";
 
 export const ButtonLink = ({
@@ -14,61 +14,64 @@ export const ButtonLink = ({
   href,
   ...props
 }: IButtonLink) => {
+  const router = useRouter();
+
+  const handleClick = (e: React.MouseEvent<HTMLAnchorElement>) => {
+    e.preventDefault();
+
+    router.push(`${href}`);
+  };
+
   if (arrow !== "none") {
     return (
-      <motion.span
+      <motion.a
         whileHover={{ scale: 1.05 }}
         whileTap={{ scale: 0.9 }}
-        className={className}
-        {...props}
-      >
-        <Link
-          href={href}
-          className={classNames(
-            "flex min-h-[40px] w-fit items-center justify-between gap-x-[10px] rounded-[5px] border pl-[11px] pr-[9px] text-[14px] font-normal leading-[19px] focus:outline-none focus-visible:outline-[#7653FC]",
-            {
-              ["border-[#7653FC] bg-[#7653FC] text-white"]:
-                appearance === "primary",
-              ["border-[#EBEBEB] bg-white text-[#3B434E]"]:
-                appearance === "secondary",
-            }
-          )}
-        >
-          <span>{children}</span>
-          <ArrowRightIcon
-            className={classNames(
-              "h-auto w-[16px] transition-transform duration-300 ease-in-out",
-              {
-                ["rotate-90"]: arrow === "down",
-              }
-            )}
-          />
-        </Link>
-      </motion.span>
-    );
-  }
-
-  return (
-    <motion.span
-      whileHover={{ scale: 1.05 }}
-      whileTap={{ scale: 0.9 }}
-      {...props}
-      className={className}
-    >
-      <Link
         href={href}
+        onClick={handleClick}
         className={classNames(
-          "flex min-h-[40px] w-fit items-center justify-center rounded-[5px] border px-[11px] text-[14px] font-normal leading-[19px] focus:outline-none focus-visible:outline-[#7653FC]",
+          "flex min-h-[40px] w-fit items-center justify-between gap-x-[10px] rounded-[5px] border pl-[11px] pr-[9px] text-[14px] font-normal leading-[19px] focus:outline-none focus-visible:outline-[#7653FC]",
           {
             ["border-[#7653FC] bg-[#7653FC] text-white"]:
               appearance === "primary",
             ["border-[#EBEBEB] bg-white text-[#3B434E]"]:
               appearance === "secondary",
-          }
+          },
+          className
         )}
+        {...props}
       >
-        {children}
-      </Link>
-    </motion.span>
+        <span>{children}</span>
+        <ArrowRightIcon
+          className={classNames(
+            "h-auto w-[16px] transition-transform duration-300 ease-in-out",
+            {
+              ["rotate-90"]: arrow === "down",
+            }
+          )}
+        />
+      </motion.a>
+    );
+  }
+
+  return (
+    <motion.a
+      href={href}
+      whileHover={{ scale: 1.05 }}
+      whileTap={{ scale: 0.9 }}
+      onClick={handleClick}
+      {...props}
+      className={classNames(
+        "flex min-h-[40px] w-fit items-center justify-center rounded-[5px] border px-[11px] text-[14px] font-normal leading-[19px] focus:outline-none focus-visible:outline-[#7653FC]",
+        {
+          ["border-[#7653FC] bg-[#7653FC] text-white"]:
+            appearance === "primary",
+          ["border-[#EBEBEB] bg-white text-[#3B434E]"]:
+            appearance === "secondary",
+        }
+      )}
+    >
+      {children}
+    </motion.a>
   );
 };
